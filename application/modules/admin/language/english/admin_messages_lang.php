51812ba7229de90f355111e9dc3260032ecfe554
John <dark-reconix@live.co.uk>
2012-02-19 14:01:04 +0000

    You have a thing with doing being, instead of been, some thing is being updates, some thing has been updated lol.

--- application/modules/admin/language/english/admin_messages_lang.php
+++ application/modules/admin/language/english/admin_messages_lang.php
@@ -1,43 +1,67 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/**
-* Name:  Admin Language - English
-* 
-* Author: Chris Baines
-* 		  chris@doveforums.com
-* 
-* Location: http://www.doveforums.com
-*          
-* Created:  27/01/2011
-* Updated:  27/01/2011
-* 
-* Description:  English language file for Messages in the Admin Panel
-* 
-*/
-
-//Success Messages
-$lang['userDeactivated'] = 'The user has being de-activated.';
-$lang['userActivated'] = 'The user has being activated.';
-$lang['userUpdated'] = 'The user has being updated.';
-$lang['categoryDeactivated'] = 'The category has being de-activated.';
-$lang['categoryActivated'] = 'The category has being activated.';
-$lang['categoryUpdated'] = 'The category has being updated.';
-$lang['categoryDeleted'] = 'The category has being deleted.';
-$lang['discussionDeactivated'] = 'The discussion has being de-activated.';
-$lang['discussionActivated'] = 'The discussion has being activated.';
-$lang['discussionStickyOff'] = 'The discussion is no longer a sticky.';
-$lang['discussionStickyOn'] = 'The discussion is now a sticky.';
-$lang['discussionOpen'] = 'The discussion is now open for posting.';
-$lang['discussionClosed'] = 'The discussion is now closed for posting.';
-$lang['discussionFlaggedOff'] = 'The discussion is no longer flagged.';
-$lang['discussionFlaggedOn'] = 'The discussion has being flagged.';
-$lang['discussionRemoved'] = 'The discussion has being removed.';
-$lang['settingsUpdated'] = 'Site settings have being updated.';
-$lang['userCreated'] = 'The user has being created. The users account details have being emailed to them.';
-$lang['deleteUser'] = 'The user has being deleted.';
-$lang['categoryCreated'] = 'The category has being created.';
-
-//Error Messages
-$lang['generalError'] = 'Something has gone wrong, please try again.';
-$lang['errorLoginRequired'] = 'Login is required to perform this action!';
-$lang['errorAdminRequired'] = 'You must be a admin to view this page!';
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* Name:  Admin Language - English
+* 
+* Author: Chris Baines
+*   	  chris@doveforums.com
+* 
+* Location: http://www.doveforums.com
+*          
+* Created:  27/01/2011
+* Updated:  19/02/2012
+* 
+* Description:  English language file for Messages in the Admin Panel
+* 
+*/
+
+//Success Messages
+$lang['userDeactivated'] = 'The user has been de-activated.';
+
+$lang['userActivated'] = 'The user has been activated.';
+
+$lang['userUpdated'] = 'The user has been updated.';
+
+$lang['categoryDeactivated'] = 'The category has been de-activated.';
+
+$lang['categoryActivated'] = 'The category has been activated.';
+
+$lang['categoryUpdated'] = 'The category has been updated.';
+
+$lang['categoryDeleted'] = 'The category has been deleted.';
+
+$lang['discussionDeactivated'] = 'The discussion has been de-activated.';
+
+$lang['discussionActivated'] = 'The discussion has been activated.';
+
+$lang['discussionStickyOff'] = 'The discussion is no longer a sticky.';
+
+$lang['discussionStickyOn'] = 'The discussion is now a sticky.';
+
+$lang['discussionOpen'] = 'The discussion is now open for posting.';
+
+$lang['discussionClosed'] = 'The discussion is now closed for posting.';
+
+$lang['discussionFlaggedOff'] = 'The discussion is no longer flagged.';
+
+$lang['discussionFlaggedOn'] = 'The discussion has been flagged.';
+
+$lang['discussionRemoved'] = 'The discussion has been removed.';
+
+$lang['settingsUpdated'] = 'Site settings have been updated.';
+
+$lang['userCreated'] = 'The user has been created and details emailed to them.';
+
+$lang['deleteUser'] = 'The user has been deleted.';
+
+$lang['categoryCreated'] = 'The category has been created.';
+
+//Error Messages
+
+$lang['generalError'] = 'Something has gone wrong, please try again.';
+
+$lang['errorLoginRequired'] = 'Login is required to perform this action!';
+
+$lang['errorAdminRequired'] = 'You must be a admin to view this page!';
+
 $lang['settingsUpdateFailed'] = 'Updating settings has failed.';
