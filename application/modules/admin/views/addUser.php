3831235b7886e3562ea4b37df8c31749ec7e1240
John <dark-reconix@live.co.uk>
2012-02-19 21:29:04 +0000

    Update application/modules/admin/views/addUser.php

--- application/modules/admin/views/addUser.php
+++ application/modules/admin/views/addUser.php
@@ -6,7 +6,7 @@
 *
 * @author			Chris Baines
 * @package			Dove Forums
-* @copyright		© 2010 - 2011 Dove Forums
+* @copyright		ï¿½ 2010 - 2011 Dove Forums
 * @last modified	03/02/2011
 **/
 ?>
@@ -20,7 +20,7 @@
     <li>
         <h3><?php echo $this->lang->line('userDetails'); ?></h3>
         <p><?php echo $this->lang->line('username'); ?><br><?php echo form_input($username); ?>&nbsp;&nbsp;<cite>Enter Users Username.</cite></p>
-        <p><?php echo $this->lang->line('password'); ?><br><?php echo form_input($password); ?>&nbsp;&nbsp;<cite>Users Password. <strong>Note - This password has being generated automatically.</strong></cite></p>
+        <p><?php echo $this->lang->line('password'); ?><br><?php echo form_input($password); ?>&nbsp;&nbsp;<cite>Users Password. <strong>Note - This password is generated automatically.</strong></cite></p>
         <p><?php echo $this->lang->line('firstName'); ?><br><?php echo form_input($first_name); ?>&nbsp;&nbsp;<cite>Enter Users First Name.</cite></p>
         <p><?php echo $this->lang->line('lastName'); ?><br><?php echo form_input($last_name); ?>&nbsp;&nbsp;<cite>Enter Users Last Name.</cite></p>
         <p><?php echo $this->lang->line('userEmail'); ?><br><?php echo form_input($email); ?>&nbsp;&nbsp;<cite>Enter Users Email Address.</cite></p>
